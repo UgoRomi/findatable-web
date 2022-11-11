47444551f1c5eec9fae5a2cb7c2e9bcdb550a3e3
Ugo Romi <ugo.romi@gmail.com>
2022-11-11 18:27:45 +0100

    Fixed issue with header on mobile Added searchbar component (WIP)

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 
 export default function Header() {
   return (
-    <header className='w-screen font-heading bg-green-50 flex justify-between items-center px-3 h-[65px]'>
+    <header className='w-screen font-heading bg-green-50 flex justify-between items-center px-3 h-[65px] border-b-4 border-green-500'>
       <Logo />
       <UserIcon className='h-6 w-6 fill-zinc-800' />
     </header>
---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { MagnifyingGlassIcon, XCircleIcon } from '@heroicons/react/24/solid';
+
+export default function SearchBar() {
+  return (
+    <div>
+      <label htmlFor='search' className='sr-only'>
+        Search for a restaurant
+      </label>
+      <div className='relative mt-1 rounded-md shadow-sm'>
+        <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
+          <MagnifyingGlassIcon
+            className='h-5 w-5 text-gray-400'
+            aria-hidden='true'
+          />
+        </div>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          className='block w-full rounded-md text-zinc-900 border-gray-300 px-10 focus:border-green-200 focus:ring-green-200 sm:text-sm'
+        />
+        <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3'>
+          <XCircleIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+        </div>
+      </div>
+    </div>
+  );
+}
