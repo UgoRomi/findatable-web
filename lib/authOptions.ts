11b030053d45ea82a2d79c918ad13e7ea5aed3fc
Ugo Romi <ugo.romi@gmail.com>
2022-11-16 00:13:33 +0100

    Worked on auth Added Prisma adapted to Next Auth Added Email Provider to signin options Added nextauth schema to prisma

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,9 +1,17 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import EmailProvider from 'next-auth/providers/email';
+import prisma from '@/lib/prismadb';
+
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
--- process.d.ts
+++ process.d.ts
@@ -8,5 +8,7 @@ declare namespace NodeJS {
     GITHUB_SECRET: string;
     GOOGLE_ID: string;
     GOOGLE_SECRET: string;
+    EMAIL_SERVER: string;
+    EMAIL_FROM: string;
   }
 }
