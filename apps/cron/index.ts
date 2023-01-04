e9d748aa2aeb72740c0fafc93cb2727a3d21647e
Ugo Romi <ugo.romi@gmail.com>
2023-01-04 23:16:52 +0100

    chore: added cron task to turbo.json

--- apps/cron/index.ts
+++ apps/cron/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { prisma } from 'database';
+import { PrismaClient } from 'database';
 async function main() {
+  const prisma = new PrismaClient();
   const restaurants = await prisma.restaurant.findMany();
   for (const restaurant of restaurants) {
     console.log(restaurant.name);
